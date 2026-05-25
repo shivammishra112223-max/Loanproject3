267a68c96af03cbdffa216816d0f61a30c4f0b0c
Shivam Mishra <shivammishra112223@gmail.com>
2026-05-25 09:53:41 +0530

    new data

--- app.py
+++ app.py
@@ -242,7 +242,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
     
-    print("hello shivam")
-    print("shubham")
-    print("welcome to system of github ")
-    print("hello shivam ")
+    
