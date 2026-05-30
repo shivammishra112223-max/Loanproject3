a1dce8e5a63329f44a4885146272b0b882618a32
Shivam Mishra <shivammishra112223@gmail.com>
2026-05-30 16:30:32 +0530

    updated file

--- app.py
+++ app.py
@@ -311,4 +311,8 @@ def apply_loan():
 # -------------------------------
 if __name__ == "__main__":
 
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
